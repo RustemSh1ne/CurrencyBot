83eb73dca0be4de0bc421f5001195a483be0bfb9
rustemsh1ne <rustemsh@mail.ru>
2022-12-27 12:48:53 +0500

    Currency bot "Mr McDee is speaking" (homework 18.6)

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "5949107867:AAFkvAj1keqHgBMW0f3luVTmSYE1OHFnlVA"
+
+keys = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'иена': 'JPY',
+    'фунт': 'GBP',
+    'франк': 'CHF',
+}
+
